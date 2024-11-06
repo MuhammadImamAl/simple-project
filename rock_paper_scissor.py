395b8fdb9adfd73864339f80392dea71d098dd82
Imam <imam.alfarizi2807@gmail.com>
2024-11-06 09:28:35 +0700

    Refactoring rock paper scissor game code

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -6,24 +6,42 @@ emojis = {
     's': '✂'
 }
 choices = ('r', 'p', 's')
-while True:
-    player = input('\nRock, paper or scissor, (r/p/s)?: ').lower()
-    cpu = random.choice(choices)
-    if player not in choices:
-        print('Invalid choices!')
-        continue
-    
+
+def get_user_choice():
+    while True:
+        player = input('\nRock, paper or scissor, (r/p/s)?: ').lower()
+        if player in choices:
+            return player
+        else:
+            print('Invalid choice!')
+
+def displaying_choice(player, cpu):
     print(f'{emojis[player]}')
     print(f'{emojis[cpu]}')
+
+def determine_winner(player, cpu):
     if player == cpu:
         print('Tie!')
     elif (player == 'r' and cpu == 's') or (player == 'p' and cpu == 'r') or (player == 's' and cpu == 'p'):
         print('You win!')
     else:
         print('You lose.')
-    
-    replay = input('\nYou want to play again? (y/n): ').lower()
-    if replay == 'y':
-        continue
-    else:
-        break
+
+def play_game():
+    while True:
+        player = get_user_choice()
+        
+        cpu = random.choice(choices)
+
+        displaying_choice(player, cpu)
+        
+        determine_winner(player, cpu)
+        
+        
+        replay = input('\nYou want to play again? (y/n): ').lower()
+        if replay == 'y':
+            continue
+        else:
+            break
+
+play_game()
